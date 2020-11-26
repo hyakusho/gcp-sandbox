d59320585549b3c0b9ecd6341065ab685bb9557c
hyakusho <hyakusho.io@gmail.com>
2020-11-26 20:30:21 +0900

    ansibleサービスアカウント追加

--- bin/create_service_accounts.sh
+++ bin/create_service_accounts.sh
@@ -1,55 +1,54 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 BASEDIR=$(cd $(dirname $0)/.. && pwd && cd - >& /dev/null)
 PROJECT=$(gcloud config get-value core/project)
-LOGFILE=/tmp/$(basename ${0}).log
+LOGFILE=/tmp/$(basename ${0}).log.$$
 
-output_logfile() {
-  cat $LOGFILE
-}
+create_service_account() {
+  service_account=$1
+  echo "$service_account"
 
-exists_service_account() {
-  SERVICE_ACCOUNT=$1
-  gcloud iam service-accounts describe ${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com > /dev/null 2>&1
-}
+  roles=()
+  case $service_account in
+    terraform)
+      roles=(compute.admin)
+      ;;
+    bigquery)
+      roles=(bigquery.dataEditor bigquery.jobUser)
+      ;;
+    ansible)
+      roles=(compute.osAdminLogin compute.instanceAdmin compute.instanceAdmin.v1)
+      ;;
+    *)
+      echo "NotSupportedServiceAccountException : $service_account"
+      exit 1
+  esac
 
-create_service_account_bigquery() {
-  SERVICE_ACCOUNT=bigquery
-  exists_service_account $SERVICE_ACCOUNT
+  # exists?
+  gcloud iam service-accounts describe ${service_account}@${PROJECT}.iam.gserviceaccount.com > /dev/null 2>&1
   if [ $? -ne 0 ]; then
-    gcloud iam service-accounts create $SERVICE_ACCOUNT --display-name $SERVICE_ACCOUNT
-    gcloud iam service-accounts keys create $BASEDIR/credentials/${SERVICE_ACCOUNT}-key.json \
-      --iam-account ${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com
+    # no exists (create a service account)
+    gcloud iam service-accounts create $service_account --display-name $service_account
+    gcloud iam service-accounts keys create $BASEDIR/credentials/${service_account}-key.json \
+      --iam-account ${service_account}@${PROJECT}.iam.gserviceaccount.com
   fi
 
+  # create/update iam-roles
   set -eo pipefail
-  gcloud projects add-iam-policy-binding $PROJECT \
-    --member serviceAccount:${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com \
-    --role roles/bigquery.dataEditor > $LOGFILE
-  gcloud projects add-iam-policy-binding $PROJECT \
-    --member serviceAccount:${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com \
-    --role roles/bigquery.jobUser > $LOGFILE
-}
-
-create_service_account_terraform() {
-  SERVICE_ACCOUNT=terraform
-  exists_service_account $SERVICE_ACCOUNT
-  if [ $? -ne 0 ]; then
-    gcloud iam service-accounts create $SERVICE_ACCOUNT --display-name $SERVICE_ACCOUNT
-    gcloud iam service-accounts keys create $BASEDIR/credentials/${SERVICE_ACCOUNT}-key.json \
-      --iam-account ${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com
-  fi
-
-  set -eo pipefail
-  gcloud projects add-iam-policy-binding $PROJECT \
-    --member serviceAccount:${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com \
-    --role roles/compute.admin > $LOGFILE
+  for role in "${roles[@]}"; do
+    echo "- $role"
+    gcloud projects add-iam-policy-binding $PROJECT \
+      --member serviceAccount:${service_account}@${PROJECT}.iam.gserviceaccount.com \
+      --role roles/$role >> $LOGFILE 2>&1
+  done
+  set +eo pipefail
 }
 
 main() {
-  create_service_account_terraform
-  create_service_account_bigquery
-  output_logfile
+  service_accounts=(terraform bigquery ansible)
+  for i in "${service_accounts[@]}"; do
+    create_service_account $i
+  done 
 }
 
 main
