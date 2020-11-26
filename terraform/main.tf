ef74930fa945a06ee474eb963fa448d3a98e0946
hyakusho <hyakusho.io@gmail.com>
2020-11-26 20:31:08 +0900

    f1-micro(無料枠)インスタンス追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+variable credentials {}
+variable project {}
+variable region { default = "us-central1" }
+variable zone { default = "use-central1-c" }
+
 terraform {
   required_version = "= 0.13.5"
   required_providers {
@@ -15,3 +20,35 @@ provider "google" {
   zone = var.zone
 }
 
+data "google_compute_image" "ubuntu_2004" {
+  family = "ubuntu-2004-lts"
+  project = "ubuntu-os-cloud"
+}
+
+resource "google_compute_instance" "default" {
+  name = "free"
+  machine_type = "f1-micro"
+  zone = var.zone
+
+  allow_stopping_for_update = true
+  labels = {
+    Environment = "development"
+    Name = "free"
+    Region = var.region
+    Zone = var.zone
+  }
+
+  boot_disk {
+    initialize_params {
+      size = 30
+      type = "pd-standard"
+      image = data.google_compute_image.ubuntu_2004.self_link
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {}
+  }
+}
--- terraform/variables.tf
+++
@@ -1,4 +0,0 @@
-variable credentials {}
-variable project {}
-variable region { default = "us-central1" }
-variable zone { default = "use-central1-c" }
