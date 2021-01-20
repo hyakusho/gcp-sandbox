a34cf237b4a3427a2125c271e493aac47ebf266f
hyakusho <hyakusho.io@gmail.com>
2021-01-20 09:41:01 +0900

    Delete a datadog instance and bigquery bukcet

--- terraform/main.tf
+++ terraform/main.tf
@@ -61,54 +61,54 @@ resource "google_compute_instance" "free" {
   }
 }
 
-resource "google_compute_instance" "datadog" {
-  name = "datadog"
-  machine_type = "e2-small"
-  zone = var.zone
-
-  allow_stopping_for_update = true
-  labels = {
-    environment = "development"
-    name = "datadog"
-    region = var.region
-    zone = var.zone
-  }
-
-  boot_disk {
-    initialize_params {
-      size = 30
-      type = "pd-standard"
-      image = data.google_compute_image.ubuntu_2004.self_link
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {}
-  }
-
-  scheduling {
-    preemptible = true
-    automatic_restart = false
-  }
-}
-
-resource "google_storage_bucket" "bigquery" {
-  name = "hyakusho-io-bigquery"
-  location = var.region
-  force_destroy = true
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled = true
-  }
-}
-
-resource "google_storage_bucket_iam_binding" "bigquery" {
-  bucket = google_storage_bucket.bigquery.name
-  role = "roles/storage.objectAdmin"
-  members = [
-    "serviceAccount:bigquery@${var.project}.iam.gserviceaccount.com"
-  ]
-}
+#resource "google_compute_instance" "datadog" {
+#  name = "datadog"
+#  machine_type = "e2-small"
+#  zone = var.zone
+#
+#  allow_stopping_for_update = true
+#  labels = {
+#    environment = "development"
+#    name = "datadog"
+#    region = var.region
+#    zone = var.zone
+#  }
+#
+#  boot_disk {
+#    initialize_params {
+#      size = 30
+#      type = "pd-standard"
+#      image = data.google_compute_image.ubuntu_2004.self_link
+#    }
+#  }
+#
+#  network_interface {
+#    network = "default"
+#
+#    access_config {}
+#  }
+#
+#  scheduling {
+#    preemptible = true
+#    automatic_restart = false
+#  }
+#}
+#
+#resource "google_storage_bucket" "bigquery" {
+#  name = "hyakusho-io-bigquery"
+#  location = var.region
+#  force_destroy = true
+#  uniform_bucket_level_access = true
+#
+#  versioning {
+#    enabled = true
+#  }
+#}
+#
+#resource "google_storage_bucket_iam_binding" "bigquery" {
+#  bucket = google_storage_bucket.bigquery.name
+#  role = "roles/storage.objectAdmin"
+#  members = [
+#    "serviceAccount:bigquery@${var.project}.iam.gserviceaccount.com"
+#  ]
+#}
